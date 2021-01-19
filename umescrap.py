3ee1ae8100bf4872963374b0c7acd5e18a7c024d
OnoRyota <67791035+OnoRyota@users.noreply.github.com>
2021-01-19 20:13:31 +0900

    Update umescrap.py

--- umescrap.py
+++ umescrap.py
@@ -1,6 +1,10 @@
 import pandas #pandasをインポート
 import pprint
 
+
+import requests
+from bs4 import BeautifulSoup
+
 #取得したいページのurlを入力
 url = 'http://www.jma.go.jp/jp/amedas_h/today-83431.html'
 
@@ -11,8 +15,8 @@ fetched_daraframes = pandas.io.html.read_html(url)
 fetched_daraframes[4].to_json('UmeNoAme.json',force_ascii=False)
 
 #プリントしたい場合
-ret = fetched_daraframes[4].to_json(force_ascii=False)
-pprint.pprint(ret)
+#ret = fetched_daraframes[4].to_json(force_ascii=False)
+#pprint.pprint(ret)
 
 #日付取得
 html = requests.get(url).content
